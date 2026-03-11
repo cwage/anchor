6b2b028bd85042eda8c7079d28e881f10260f00e
Chris Wage <cwage@quietlife.net>
2026-03-10 21:09:26 -0500

    Bump version to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.anchor.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.4.0"
     }
 
     buildTypes {
